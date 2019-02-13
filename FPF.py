8ddd184496c33337e9a41ea1cff363c34808caf0
hanson-hschang <hanson.hschang@gmail.com>
2019-02-13 12:50:52 -0600

    add method run in class FPF and add class Figure

--- FPF.py
+++ FPF.py
@@ -7,6 +7,10 @@ Created on Wed Jan. 23, 2019
 import numpy as np
 from scipy.signal import find_peaks
 
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
 class Particles(object):
     """
     Particles: particles has a scalar state which denotes phase in [0 2pi] cycle.
@@ -39,7 +43,6 @@ class Particles(object):
         self.Ki = 0.01
         self.sync_matrix = np.zeros([self.N, self.N])
         self.update_sync_matrix()
-
     
     def update(self, theta_error):
         """mod the angle of each particle with 2pi"""
@@ -218,6 +221,20 @@ class FPF(object):
         self.particles.update(theta_error=dtheta-self.particles.omega*self.dt)
         return
 
+    def run(self, y):
+        h_hat = np.zeros(y.shape)
+        theta = np.zeros([self.particles.N, y.shape[1]])
+        amp = np.zeros([self.particles.N, y.shape[1]])
+        freq_hat = np.zeros([self.particles.N, y.shape[1]])
+
+        for k in range(y.shape[1]):
+            self.update(y[:,k])
+            h_hat[:,k] = self.h_hat
+            theta[:,k] = self.particles.get_theta()
+            amp[:,k] = self.particles.get_amp()
+            freq_hat[:,k] = self.particles.get_freq()
+        return Struct(h_hat=h_hat, theta=theta, amp=amp, freq_hat=freq_hat)
+
 class Signal(object):
     """
     Signal:
@@ -266,6 +283,96 @@ class Signal(object):
         """observation function maps states(x) to observations(y)"""
         return np.matmul(amp, np.sin(x))
 
+class Struct(dict):
+    def __init__(self, **kwds):
+        dict.__init__(self, kwds)
+        self.__dict__.update(kwds)
+
+class Figure(object):
+    def __init__(self, fig_property, signal, filtered_signal):
+        self.fig_property = fig_property
+        self.signal = signal
+        self.filtered_signal = filtered_signal
+    
+    def plot(self):
+
+        if self.fig_property.plot_signal:
+            fig, axes = plt.subplots(self.signal.Y.shape[0], 1, figsize=(9,7))
+            signal_axes = self.plot_signal(axes)
+        
+        if self.fig_property.plot_theta:
+            fig, ax = plt.subplots(1,1, figsize=(9,7))
+            theta_ax = self.plot_theta(ax)
+        
+        if self.fig_property.plot_omega:
+            fig, ax = plt.subplots(1,1, figsize=(9,7))
+            omega_ax = self.plot_omega(ax)
+        
+        if self.fig_property.plot_amp:
+            fig, ax = plt.subplots(1,1, figsize=(9,7))
+            amp_ax = self.plot_amp(ax)
+
+        if self.fig_property.show:
+            plt.show()
+        
+        return
+
+    def plot_signal(self, axes):
+        try:
+            iterator = iter(axes)
+        except TypeError:
+            ax = axes
+            ax.plot(self.signal.t, self.signal.Y[0], label=r'signal')
+            ax.plot(self.signal.t, self.filtered_signal.h_hat[0,:], label=r'estimation')
+            ax.legend(fontsize=self.fig_property.fontsize-5)
+            ax.tick_params(labelsize=self.fig_property.fontsize)
+            ax.set_xlabel('time', fontsize=self.fig_property.fontsize)
+        else:
+            for i, ax in enumerate(axes):
+                ax.plot(self.signal.t, self.signal.Y[i], label=r'signal')
+                ax.plot(self.signal.t, self.filtered_signal.h_hat[i,:], label=r'estimation')
+                ax.legend(fontsize=self.fig_property.fontsize-5)
+                ax.tick_params(labelsize=self.fig_property.fontsize)
+                ax.set_xlabel('time', fontsize=self.fig_property.fontsize)
+        return axes
+
+    def plot_theta(self, ax):
+        for k in range(self.filtered_signal.theta.shape[0]):
+            ax.scatter(self.signal.t, self.filtered_signal.theta[k,:], s=0.5)
+        ax.tick_params(labelsize=self.fig_property.fontsize)
+        ax.set_xlabel('time', fontsize=self.fig_property.fontsize)
+        ax.set_ylabel(r'$\theta$ [rad]', fontsize=self.fig_property.fontsize)
+        ax.set_title('Particles', fontsize=self.fig_property.fontsize+2)
+        return ax
+    
+    def plot_omega(self, ax):
+        for k in range(self.filtered_signal.freq_hat.shape[0]):
+            ax.plot(self.signal.t, self.filtered_signal.freq_hat[k,:]) 
+        ax.tick_params(labelsize=self.fig_property.fontsize)
+        ax.set_xlabel('time', fontsize=self.fig_property.fontsize)
+        ax.set_ylabel('$\omega$ [Hz]', fontsize=self.fig_property.fontsize)
+        ax.set_title('Frequency of Particles', fontsize=self.fig_property.fontsize+2)
+        
+        return ax
+    
+    def plot_amp(self, ax):
+        for k in range(self.filtered_signal.amp.shape[0]):
+            ax.plot(self.signal.t, self.filtered_signal.amp[k,:]) 
+        ax.tick_params(labelsize=self.fig_property.fontsize)
+        ax.set_xlabel('time', fontsize=self.fig_property.fontsize)
+        ax.set_ylabel('$A$', fontsize=self.fig_property.fontsize)
+        ax.set_title('Amplitude of Particles', fontsize=self.fig_property.fontsize+2)
+        return ax
+    
+    def plot_sync_matrix(self, ax, sync_matrix):
+        ax.pcolor(np.arange(1,sync_matrix.shape[0]+1), np.arange(1,sync_matrix.shape[0]+1),\
+                    sync_matrix, cmap='gray', vmin=0, vmax=1)
+        return ax
+    
+    def plot_sync_particle(self, ax, sync_particle):
+        ax.plot(sync_particle)
+        return ax
+
 def h(amp, x):
         return amp*np.cos(x)
 
@@ -279,75 +386,26 @@ if __name__ == "__main__":
     # M states of signal noises
     sigma_W = [0.1]
 
-    T = 10.
+    T = 2.
     dt = 0.001
     signal = Signal(freq=freq, amp=amp, sigma_B=sigma_B, sigma_W=sigma_W, dt=dt, T=T)
-    h_hat = np.zeros(signal.Y.shape)
+    # h_hat = np.zeros(signal.Y.shape)
     
     N=100
     feedback_particle_filter = FPF(number_of_particles=N, f_min=0.9, f_max=1.1, sigma_W=sigma_W, dt=dt, h=h)
-    theta = np.zeros([N, signal.Y.shape[1]])
-    amp = np.zeros([N, signal.Y.shape[1]])
-    freq_hat = np.zeros([N, signal.Y.shape[1]])
+    filtered_signal = feedback_particle_filter.run(signal.Y)
 
-    for k in range(signal.Y.shape[1]):
-        feedback_particle_filter.update(signal.Y[:,k])
-        h_hat[:,k] = feedback_particle_filter.h_hat
-        theta[:,k] = feedback_particle_filter.particles.get_theta()
-        amp[:,k] = np.squeeze(feedback_particle_filter.particles.amp)
-        freq_hat[:,k] = feedback_particle_filter.particles.get_freq()
-
-    import matplotlib
-    matplotlib.use("TkAgg")
-    import matplotlib.pyplot as plt
-    # plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
     fontsize = 20
-    fig, axes = plt.subplots(signal.Y.shape[0],1, figsize=(9,7))
-    try:
-        iterator = iter(axes)
-    except TypeError:
-        ax = axes
-        ax.plot(signal.t, signal.Y[0], label=r'signal')
-        ax.plot(signal.t, h_hat[0,:], label=r'estimation')
-    else:
-        for i, ax in enumerate(axes):
-            ax.plot(signal.t, signal.Y[i], label=r'signal')
-            ax.plot(signal.t, h_hat[i,:], label=r'estimation')
-    ax.legend(fontsize=fontsize-5)
-    ax.tick_params(labelsize=fontsize)
-    ax.set_xlabel('time', fontsize=fontsize)
+    fig_property = Struct(fontsize=fontsize, show=False, \
+                            plot_signal=True, plot_theta=True, plot_omega=True, plot_amp=True)
+    figure = Figure(fig_property=fig_property, signal=signal, filtered_signal=filtered_signal)
+    figure.plot()
 
-    fig, ax = plt.subplots(1,1, figsize=(9,7))
-    for k in range(N):
-        ax.scatter(signal.t, theta[k,:], s=0.5)
-    ax.tick_params(labelsize=fontsize)
-    ax.set_xlabel('time', fontsize=fontsize)
-    ax.set_ylabel(r'$\theta$ [rad]', fontsize=fontsize)
-    ax.set_title('Particles', fontsize=fontsize+2)
-    
-    fig, ax = plt.subplots(1,1, figsize=(9,7))
-    for k in range(N):
-        ax.plot(signal.t, amp[k,:]) 
-    ax.tick_params(labelsize=fontsize)
-    ax.set_xlabel('time', fontsize=fontsize)
-    ax.set_ylabel('$A$', fontsize=fontsize)
-    ax.set_title('Amplitude of Particles', fontsize=fontsize+2)
-        
-    fig, ax = plt.subplots(1,1, figsize=(9,7))
-    for k in range(N):
-        ax.plot(signal.t, freq_hat[k,:]) 
-    ax.tick_params(labelsize=fontsize)
-    ax.set_xlabel('time', fontsize=fontsize)
-    ax.set_ylabel('$\omega$ [Hz]', fontsize=fontsize)
-    ax.set_title('Frequency of Particles', fontsize=fontsize+2)
+    fig, ax = plt.subplots(1,1, figsize=(7,7))
+    ax = figure.plot_sync_matrix(ax, feedback_particle_filter.particles.update_sync_matrix())
     
     fig, ax = plt.subplots(1,1, figsize=(7,7))
-    ax.pcolor(range(N), range(N), feedback_particle_filter.particles.update_sync_matrix(), cmap='gray', vmin=0, vmax=1)
- 
-    fig, ax = plt.subplots(1,1, figsize=(7,7))
-    ax.plot(feedback_particle_filter.particles.check_sync())
-    
+    ax = figure.plot_sync_particle(ax, feedback_particle_filter.particles.check_sync())
     
     plt.show()
 
--- multi_frequencies.py
+++ multi_frequencies.py
@@ -5,7 +5,10 @@ Created on Thu Jan. 31, 2019
 """
 
 import numpy as np
-from FPF import FPF, Signal
+from FPF import FPF, Signal, Struct, Figure
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 
 def h(amp, x):
         return amp*np.cos(x)
@@ -27,44 +30,18 @@ if __name__ == "__main__":
     
     N=100
     feedback_particle_filter = FPF(number_of_particles=N, f_min=0.6, f_max=1.2, sigma_W=sigma_W, dt=dt, h=h)
-    theta = np.zeros([N, signal.Y.shape[1]])
-    freq_hat = np.zeros([N, signal.Y.shape[1]])
+    filtered_signal = feedback_particle_filter.run(signal.Y)
 
-    for k in range(signal.Y.shape[1]):
-        feedback_particle_filter.update(signal.Y[:,k])
-        h_hat[:,k] = feedback_particle_filter.h_hat
-        theta[:,k] = feedback_particle_filter.particles.get_theta()
-        freq_hat[:,k] = feedback_particle_filter.particles.get_freq()
-    feedback_particle_filter.particles.update_sync_matrix()
-
-    import matplotlib
-    matplotlib.use("TkAgg")
-    import matplotlib.pyplot as plt
-
-    fig, axes = plt.subplots(signal.Y.shape[0],1, figsize=(9,7))
-    try:
-        iterator = iter(axes)
-    except TypeError:
-        ax = axes
-        ax.plot(signal.t, signal.Y[0])
-        ax.plot(signal.t, h_hat[0,:])
-    else:
-        for i, ax in enumerate(axes):
-            ax.plot(signal.t, signal.Y[i])
-            ax.plot(signal.t, h_hat[i,:])
-    
-    fig, ax = plt.subplots(1,1, figsize=(9,7))
-    for k in range(N):
-        ax.scatter(signal.t, theta[k,:], s=0.5)
-    
-    fig, ax = plt.subplots(1,1, figsize=(9,7))
-    for k in range(N):
-        ax.plot(signal.t, freq_hat[k,:]) 
-
-    fig, ax = plt.subplots(1,1, figsize=(7,7))
-    ax.pcolor(range(N), range(N), feedback_particle_filter.particles.update_sync_matrix(), cmap='gray', vmin=0, vmax=1)
+    fontsize = 20
+    fig_property = Struct(fontsize=fontsize, show=False, \
+                            plot_signal=True, plot_theta=True, plot_omega=True, plot_amp=True)
+    figure = Figure(fig_property=fig_property, signal=signal, filtered_signal=filtered_signal)
+    figure.plot()
     
     fig, ax = plt.subplots(1,1, figsize=(7,7))
-    ax.plot(feedback_particle_filter.particles.check_sync())
+    ax = figure.plot_sync_matrix(ax, feedback_particle_filter.particles.update_sync_matrix())
+    
+    fig, ax = plt.subplots(1,1, figsize=(7,7))
+    ax = figure.plot_sync_particle(ax, feedback_particle_filter.particles.check_sync())
 
     plt.show()
