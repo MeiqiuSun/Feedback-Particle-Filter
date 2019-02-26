a0efb1e8e349dc0d55ca78f210d7b0c32b186ea3
hanson-hschang <hanson.hschang@gmail.com>
2019-02-26 12:56:41 -0600

    Update Signal.py generalization

--- linear-guassian.py
+++ linear-guassian.py
@@ -3,6 +3,7 @@ Created on Mon Feb. 25, 2019
 
 @author: Heng-Sheng (Hanson) Chang
 """
+
 from Signal import Signal, Linear
 
 import numpy as np
@@ -13,13 +14,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     
-    T = 100.
-    fs = 16
+    T = 10.
+    fs = 160
     dt = 1/fs
-    signal_type = Linear()
-    signal = Signal(a=signal_type.a, b=signal_type.b, h=signal_type.h, sigma_B=signal_type.sigma_B, sigma_W=signal_type.sigma_W, \
-                    X0=signal_type.X0, u=signal_type.u, dt=dt, T=T)
-                    
+    signal_type = Linear(dt)
+    signal = Signal(f=signal_type.f, h=signal_type.h, sigma_B=signal_type.sigma_B, sigma_W=signal_type.sigma_W, X0=signal_type.X0, dt=dt, T=T)
+
     fontsize = 20
     fig, ax = plt.subplots(1, 1, figsize=(9,7))
     for m in range(signal.Y.shape[0]):
